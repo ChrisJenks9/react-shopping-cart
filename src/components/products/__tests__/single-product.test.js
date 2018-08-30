15df9803cdaf1b5169287e78af672665d8c76849
Chris Jenks <christopher.jenks@hmhco.com>
2018-08-30 16:02:56 +0100

    test: add unit test for add to cart & out of stock button

--- src/components/products/__tests__/single-product.test.js
+++ src/components/products/__tests__/single-product.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ProductList from '../single-product';
+import { shallow, mount } from 'enzyme';
 import Database from '../../../db.json';
 import SingleProduct from '../single-product';
 
@@ -8,5 +7,16 @@ describe('#SingleProduct()', () => {
   test('renders with a single product without crashing', () => {
     const singleProduct = shallow(<SingleProduct key={Database.products[0].id} item={Database.products[0]} />);
     expect(singleProduct).toMatchSnapshot();
+  });
+
+  describe('inStock()', () => {
+    test('To return add to cart button if there is > 0 stock', () => {
+      const singleProduct = mount(<SingleProduct key={Database.products[0].id} item={Database.products[0]} />);
+      expect(singleProduct.instance().inStock()).toMatchSnapshot();
+    });
+    test('To return out of stock button if there is 0 stock', () => {
+      const singleProduct = mount(<SingleProduct key={Database.products[4].id} item={Database.products[4]} />);
+      expect(singleProduct.instance().inStock()).toMatchSnapshot();
+    });
   });
 });
